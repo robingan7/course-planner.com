4f1c721c8b40875895823c1c8326a5e4a0b2c261
Robingan7 <gzt11111@gmail.com>
2019-11-23 00:07:02 -0800

    more routes

--- src/components/manage.component.js
+++ src/components/manage.component.js
@@ -1,4 +1,11 @@
 import React, { Component } from "react";
+import PlansList from "./plansList.component";
+import BlocksAndSche from "./blocksAndSchedule.component";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import DeleteIcon from "@material-ui/icons/Delete";
+import AddIcon from "@material-ui/icons/Add";
 
 export default class Manage extends Component {
     constructor(props) {
@@ -7,9 +14,37 @@ export default class Manage extends Component {
 
     render() {
         return (
-           <div className="form-container sign-in-container regular-container">
-               <h1>Manage</h1>
-           </div>
+          <div className="regular-container">
+            <h1>Manage</h1>
+            <div className="manageContent">
+              <h2 className="subTitle">Plans</h2>
+              <PlansList name="jjj"/>
+              <Grid item className="toolKit">
+                <ButtonGroup
+                  variant="contained"
+                  color="primary"
+                  aria-label="full-width contained primary button group"
+                >
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    startIcon={<AddIcon />}
+                  >
+                    Add
+                  </Button>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    startIcon={<DeleteIcon />}
+                  >
+                    Delete
+                  </Button>
+                </ButtonGroup>
+              </Grid>
+              <h2 className="subTitle">Blocks & Textbook</h2>
+              <BlocksAndSche />
+            </div>
+          </div>
         );
     };
 }
---
+++ src/components/plansList.component.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import Checkbox from "@material-ui/core/Checkbox";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EditIcon from "@material-ui/icons/Edit";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: "100%",
+    maxWidth: 360,
+    backgroundColor: '#fff',
+    marginLeft:'10px',
+    height:'400px',
+    overflow: 'auto',
+    boxShadow: "0 18px 28px rgba(0,0,0,0.35), 0 10px 20px rgba(0,0,0,0.32)"
+  }
+}));
+
+export default function PlansList(props) {
+    console.log(props);
+  const classes = useStyles();
+  const [checked, setChecked] = React.useState([0]);
+
+  const handleToggle = value => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
+  return (
+    <List className={classes.root}>
+      {[2.2, 2.3,2.4,2.5,2.6,2.7,2.8,2.9,3.1].map(value => {
+        const labelId = `checkbox-list-label-${value}`;
+
+        return (
+          <ListItem
+            key={value}
+            role={undefined}
+            dense
+            button
+            onClick={handleToggle(value)}
+          >
+            <ListItemIcon>
+              <Checkbox
+                edge="start"
+                tabIndex={-1}
+                disableRipple
+                inputProps={{ "aria-labelledby": labelId }}
+              />
+            </ListItemIcon>
+            <ListItemText id={labelId} primary={`Unit ${value}`} />
+            <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="comments">
+                <EditIcon />
+              </IconButton>
+              <IconButton edge="end" aria-label="comments">
+                <DeleteIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+}
